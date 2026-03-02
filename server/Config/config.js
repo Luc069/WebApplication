fd3ea537ac8c227edad9346ddf11be21ac12bb90
Luc069 <91779803+Luc069@users.noreply.github.com>
2026-03-02 18:47:11 +0100

    Update config.js

--- server/Config/config.js
+++ server/Config/config.js
@@ -3,7 +3,7 @@ export const config = {
   port: 5000,
 
   // JsonWebToken
-  JwtSecretKey: "1804230cb33b8c7__::Liliinmica12::__32428047328047r832j",
+  JwtSecretKey: "",
 
   // Database
   dbhost: "localhost",
@@ -15,10 +15,10 @@ export const config = {
   // Email
   user: "apikey",
   password:
-    "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",
+    "",
   service: "SendGrid",
   from: "horvatlucian1@gmail.com",
 
   // Image hosting
-  image_hosting_api_key: "39a6bde133df4cd24f36486a6d37344d",
+  image_hosting_api_key: "",
 }
